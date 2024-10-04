fff8f44b5fe2290e0825bfcf5228a80e31dd12f8
Dvornichenko Kirill <gglade999@gmail.com>
2024-10-04 18:28:51 +0300

    Добавил кнопку для перехода на тест страницу

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,10 @@
 import { Navbar as NextUINavbar } from '@nextui-org/navbar'
+import Link from 'next/link'
 
 export const Navbar = () => {
 	return (
 		<NextUINavbar maxWidth='xl' position='sticky'>
+			<Link href={'test'}>Test</Link>
 		</NextUINavbar>
 	)
 }
