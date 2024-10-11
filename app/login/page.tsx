1bd700abbe8353199e04bb3a7908fa0b57d84989
Dvornichenko Kirill <gglade999@gmail.com>
2024-10-11 21:04:22 +0300

    Стилизовал кнопку входа

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -53,7 +53,9 @@ export default function LoginPage() {
 					</Button>
 				</>
 			) : (
-				<Button onClick={handleGoogleLogin}>Войти через Google</Button>
+				<Button color='success' onClick={handleGoogleLogin}>
+					Войти через Google
+				</Button>
 			)}
 		</div>
 	)
